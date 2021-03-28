d0eb9bfbb680172bbb06659b61da498c15d7ac0e
Daniel Trương <simongray2016@gmail.com>
2021-03-28 23:35:47 +0700

    dark mode

--- lib/modules/settings/components/dark_mode_card.dart
+++ lib/modules/settings/components/dark_mode_card.dart
@@ -4,29 +4,9 @@ import 'package:myapp/constants/colors.dart';
 import 'package:myapp/constants/theme_mode.dart';
 import 'package:myapp/cubit/theme_mode_cubit.dart';
 
-class DarkModeCard extends StatefulWidget {
-  const DarkModeCard({
-    Key key,
-  }) : super(key: key);
-
-  @override
-  _DarkModeCardState createState() => _DarkModeCardState();
-}
-
-class _DarkModeCardState extends State<DarkModeCard> {
-  bool value = false;
-
+class DarkModeCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    onChange(bool value) {
-      !value
-          ? context.read<ThemeModeCubit>().switchThemeMode(EThemeMode.light)
-          : context.read<ThemeModeCubit>().switchThemeMode(EThemeMode.dark);
-      setState(() {
-        this.value = value;
-      });
-    }
-
     return Card(
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 20),
@@ -36,16 +16,46 @@ class _DarkModeCardState extends State<DarkModeCard> {
             'Dark mode',
             style: Theme.of(context).textTheme.headline4,
           ),
-          trailing: BlocListener<ThemeModeCubit, ThemeModeState>(
-            listener: (context, state) {},
-            child: Switch(
-              value: this.value,
-              onChanged: (value) => onChange(value),
-              activeColor: PrimaryColor,
-            ),
-          ),
+          trailing: DarkModeSwitch(),
         ),
       ),
+    );
+  }
+}
+
+class DarkModeSwitch extends StatefulWidget {
+  const DarkModeSwitch({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  _DarkModeSwitchState createState() => _DarkModeSwitchState();
+}
+
+class _DarkModeSwitchState extends State<DarkModeSwitch> {
+  bool value = false;
+
+  @override
+  Widget build(BuildContext context) {
+    switchToDarkMode(bool value) {
+      setState(() {
+        this.value = value;
+      });
+      !this.value
+          ? context.read<ThemeModeCubit>().switchThemeMode(EThemeMode.light)
+          : context.read<ThemeModeCubit>().switchThemeMode(EThemeMode.dark);
+    }
+
+    return BlocBuilder<ThemeModeCubit, ThemeModeState>(
+      builder: (context, state) {
+        this.value = state.themeMode == EThemeMode.dark;
+
+        return Switch(
+          value: this.value,
+          onChanged: (value) => switchToDarkMode(value),
+          activeColor: PrimaryColor,
+        );
+      },
     );
   }
 }
